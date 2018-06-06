55b61d7b64185b7e3090426915da17edf7b5fd80
Ahmed Hamdan <ahmedhamdan9898@gmail.com>
2018-06-06 16:06:21 +0300

    The first Commit

---
+++ CalculatorPublic/src/calculatorpublic/CalculatorPublic.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatorpublic;
+import java.util.Scanner;
+/**
+ *
+ * @author Ahmad
+ */
+public class CalculatorPublic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+Scanner scn= new Scanner (System.in);
+        System.out.println("Hello, welcome to the Calculator");
+        System.out.println("Enter the 1st number");
+        int n1= scn.nextInt();
+        System.out.println("Enter the 2nd number");
+        int n2= scn.nextInt();
+        
+        
+        int result= n1+n2;
+        System.out.println("The sum is: " +result);
+        
+    }
+    
+}
